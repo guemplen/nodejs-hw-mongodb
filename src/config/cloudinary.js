16f7a00a785a933d6af5998ec8cd3bbc8ac9831a
guemplen <guemplen@gmail.com>
2024-08-21 23:34:36 +0200

    Add POST /contacts & PATCH /contacts/:contactId
    Photo upload functionality for routes
    POST /contacts
    PATCH /contacts/:contactId

---
+++ src/config/cloudinary.js
@@ -0,0 +1,25 @@
+import cloudinary from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const cloudinaryConfig = {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+};
+
+// console.log('Cloudinary Configuration:');
+// console.log('Cloud Name:', cloudinaryConfig.cloud_name);
+// console.log('API Key:', cloudinaryConfig.api_key);
+
+cloudinary.v2.config(cloudinaryConfig);
+
+// try {
+//   cloudinary.v2.config(cloudinaryConfig);
+//   console.log('Cloudinary configured successfully.');
+// } catch (error) {
+//   console.error('Error configuring Cloudinary:', error);
+// }
+
+export default cloudinary.v2;
