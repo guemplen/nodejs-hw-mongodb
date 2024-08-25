96af32e64c8c5bbfc861b10c173d870ae6cbf782
guemplen <guemplen@gmail.com>
2024-08-25 02:08:38 +0200

    Configuration & organization of API docs for various endpoints.

--- src/server.js
+++ src/server.js
@@ -8,9 +8,11 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import fs from 'fs';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
 if (!fs.existsSync(TEMP_UPLOAD_DIR)) {
   fs.mkdirSync(TEMP_UPLOAD_DIR, { recursive: true });
@@ -25,6 +27,7 @@ app.use(cookieParser());
 
 app.use('/contacts', contactsRouter);
 app.use('/auth', authRouter);
+app.use('/api-docs', swaggerDocs());
 
 app.use(notFoundHandler);
 app.use(errorHandler);
